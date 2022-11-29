bbc6ae72094115391d4c05f85b22bff7aa740520
LianPark <woori01@gmail.com>
2022-11-30 08:42:08 +0900

    initial commit

---
+++ sample.php
@@ -0,0 +1,24 @@
+﻿<?php
+$xml_doc = new DOMDocument();
+$xml_doc->load('sample.xml');
+
+print $xml_doc->saveXML(); echo "<br>-----------<br>";
+  
+$i = $xml_doc->documentElement;
+foreach ($i->childNodes AS $item) {
+	print $item->nodeName . " = " . $item->nodeValue . "<br>";
+}
+echo "<br>-----------> nodeName 출력<br>";
+
+$xpath = new DOMXpath($xml_doc);
+$nodes = $xpath->query('//*');
+
+$names = array();
+foreach ($nodes as $node)
+{
+    $names[] = $node->nodeName;
+}
+
+echo join(PHP_EOL, array_unique($names)); echo "<br>";
+print_r($names);
+?>
---
+++ test.php
@@ -0,0 +1,15 @@
+﻿<?php
+$xml = simplexml_load_file('employees.xml');
+
+echo '<h2>Employees Listing</h2>';
+
+$list = $xml->record;
+for ($i = 0; $i < count($list); $i++) {
+    echo '<b>Man no:</b> ' . $list[$i]->attributes()->man_no . '<br>';
+    echo 'Name: ' . $list[$i]->name . '<br>';
+    echo 'Position: ' . $list[$i]->position . '<br><br>';
+}
+
+echo count($list)."<br>";
+print_r($xml);
+?>
